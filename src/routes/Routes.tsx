b4dabf35284e0829e62d82bce3f089061c1d922d
SantiRosso <santiagorosso@live.com.ar>
2023-11-03 02:11:30 -0300

    initial commit

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// //components
+// import Home from '../views/Home';
+// import AddFood from '../views/AddFood';
+
+// const Routes = () => {
+//     return ( 
+//         <NavigationContainer>
+//             <Stack.Navigator initialRouteName='Home'>
+//                 <Stack.Screen name='Home' component={Home} options={{headerShown: false}}/>
+//                 <Stack.Screen name='AddFood' component={AddFood} options={{headerShown: false}}/>
+//             </Stack.Navigator>
+//         </NavigationContainer>
+//     );
+// };
+
+// export default Routes;
---
+++ src/views/AddFood/AddFood.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const AddFood = () => {
+    return(
+        <View>
+            <Text>AddFood</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default AddFood;
