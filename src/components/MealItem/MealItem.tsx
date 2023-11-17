0493054e3737171f8397740bdb4be08f6c8b823c
SantiRosso <santiagorosso@live.com.ar>
2023-11-17 13:56:25 -0300

    MealItem component

---
+++ src/components/MealItem/MealItem.tsx
@@ -0,0 +1,43 @@
+import React, {FC} from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Meal } from '../../types';
+import { Button, Icon } from '@rneui/themed';
+
+
+const MealItem: FC<Meal> = ({calories, name, portion}) => {
+    return(
+        <View style={styles.container}>
+            <View style={styles.leftContainer}>
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.portion}>{portion}</Text>
+            </View>
+            <View style={styles.rightContainer}>
+                <Button icon={<Icon name='add-circle-outline'/>} type='clear'/>
+                <Text style={styles.calories}>{calories}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    leftContainer: {
+
+    },
+    rightContainer: {
+
+    },
+    name: {
+
+    },
+    portion: {
+
+    },
+    calories: {
+        
+    }
+})
+
+export default MealItem;
--- src/views/AddFood/AddFood.tsx
+++ src/views/AddFood/AddFood.tsx
@@ -1,17 +1,38 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Alert, ScrollView } from 'react-native';
+import { Button, Icon, Input } from '@rneui/themed';
+//hooks
+import useFoodStorage from '../../hooks/useFoodStorage';
 //components
 import Header from '../../components/Header';
-import { Button, Icon, Input } from '@rneui/themed';
 import AddFoodModal from '../../components/AddFoodModal';
+//types
+import { Meal } from '../../types';
+import MealItem from '../../components/MealItem';
 
 const AddFood = () => {
 
     const [visible, setVisible] = useState<boolean>(false)
+    const [foods, setFoods] = useState<Meal[]>([])
+    const {onGetFood} = useFoodStorage();
 
-    const handleModalClose = (showUpdate?: boolean) => {
+    const loadFoods = async () => {
+        try {
+            const foodsResponse = await onGetFood();
+            setFoods(foodsResponse);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        loadFoods().catch(null);
+    }, []);
+
+    const handleModalClose = async (showUpdate?: boolean) => {
         if(showUpdate){
             Alert.alert('Comida guardad exitosamente');
+            loadFoods();
         }
         setVisible(false);
     }
@@ -35,6 +56,9 @@ const AddFood = () => {
                     <Button title='Search' color='#ade8af' titleStyle={styles.searchButtonTitle} radius='lg'/>
                 </View>
             </View>
+            <ScrollView style={styles.content}>
+                {foods?.map(meal => <MealItem key={`my-meal-item-${meal.name}`} {...meal}/>)}
+            </ScrollView>
             <AddFoodModal visible={visible} onClose={handleModalClose}/>
         </View>
     )
@@ -78,6 +102,9 @@ const styles = StyleSheet.create({
     searchButtonTitle: {
         color: '#000',
         fontSize: 14
+    },
+    content: {
+
     }
 })
 
