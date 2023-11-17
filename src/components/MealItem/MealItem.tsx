630432ca0016e37ad618f9e5e43fbdd13dd051f1
SantiRosso <santiagorosso@live.com.ar>
2023-11-17 19:23:47 -0300

    MealItem styles

--- src/components/MealItem/MealItem.tsx
+++ src/components/MealItem/MealItem.tsx
@@ -12,8 +12,8 @@ const MealItem: FC<Meal> = ({calories, name, portion}) => {
                 <Text style={styles.portion}>{portion}</Text>
             </View>
             <View style={styles.rightContainer}>
-                <Button icon={<Icon name='add-circle-outline'/>} type='clear'/>
-                <Text style={styles.calories}>{calories}</Text>
+                <Button icon={<Icon name='add-circle-outline'/>} type='clear' style={styles.iconButton}/>
+                <Text style={styles.calories}>{calories} cal</Text>
             </View>
         </View>
     )
@@ -21,22 +21,35 @@ const MealItem: FC<Meal> = ({calories, name, portion}) => {
 
 const styles = StyleSheet.create({
     container: {
-
+        backgroundColor: '#ade8af',
+        borderRadius: 12,
+        padding: 12,
+        marginBottom: 12,
+        flexDirection: 'row'
     },
     leftContainer: {
-
+        flex: 1,
+        justifyContent: 'center'
     },
     rightContainer: {
-
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end'
     },
     name: {
-
+        fontSize: 18,
+        fontWeight: '500'
     },
     portion: {
-
+        fontSize: 13,
+        color: '#808080',
+        fontWeight: '500'
     },
     calories: {
-        
+        fontSize: 18
+    },
+    iconButton: {
+        marginBottom: -8
     }
 })
 
