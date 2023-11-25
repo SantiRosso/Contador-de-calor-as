f83be6afb4e4759d722995590142ceb284ce3262
SantiRosso <santiagorosso@live.com.ar>
2023-11-25 00:56:05 -0300

    Home: useFocusEffect

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import { View, StyleSheet } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 //components
 import Header from '../../components/Header';
 import CaloriesModule from '../../components/CaloriesModule';
+//hooks
+import useFoodStorage from '../../hooks/useFoodStorage';
+//types
+import { Meal } from '../../types';
 
 const Home = () => {
+
+    const { onGetTodayFood } = useFoodStorage();
+    const [todayFood, setTodayFood] = useState<Meal[]>([]);
+
+    const loadTodayFood = useCallback(async () => {
+        try {
+            const todayFoodResponse = await onGetTodayFood();
+            setTodayFood(todayFoodResponse);
+        } catch (error) {
+            setTodayFood([]);
+            console.error(error);
+        }
+    }, []);
+
+    useFocusEffect(useCallback(() => {
+        loadTodayFood().catch(null);
+    }, [loadTodayFood]));
+
     return(
         <View style={styles.container}>
             <Header />
             <CaloriesModule />
         </View>
     )
-}
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -19,6 +42,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         padding: 12,
     }
-})
+});
 
 export default Home;
